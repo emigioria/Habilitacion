28225adc90b571bd1705da470cb3d4ed15266a9e
Emiliano <Emiliano@NoteGioria>
2016-05-12 19:45:31 -0300

    Probando

--- src/proy/gui/Main.java
+++ src/proy/gui/Main.java
@@ -1,5 +1,14 @@
 package proy.gui;
 
+import java.util.ArrayList;
+import java.util.Vector;
+
 public class Main {
 
+	public static void main(String[] args) {
+		Vector<Boolean> v = new Vector<Boolean>();
+		ArrayList<Boolean> a = new ArrayList<Boolean>();
+		v.add(true);
+		a.add(true);
+	}
 }
