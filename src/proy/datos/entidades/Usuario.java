dea2621f0b8996747326876dc53fd4d50993cda7
Emiliano <Emiliano@NoteGioria.local>
2016-05-15 17:53:14 -0300

    Cambio menor

--- src/proy/datos/entidades/Usuario.java
+++ src/proy/datos/entidades/Usuario.java
@@ -20,7 +20,7 @@ import proy.datos.clases.Estado;
 public abstract class Usuario {
 
 	@Id
-	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	@GeneratedValue(strategy = GenerationType.AUTO)
 	@Column(name = "codigo")
 	private Integer codigo;
 
